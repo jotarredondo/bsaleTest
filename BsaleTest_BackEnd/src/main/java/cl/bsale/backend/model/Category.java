d1ca2092941c2a4c6c0073f9df00f8a00182634c
Jose_Arredondo <jotarredondo@gmail.com>
2020-11-18 12:11:56 -0300

    Creación Entidades

---
+++ BsaleTest_BackEnd/src/main/java/cl/bsale/backend/model/Category.java
@@ -0,0 +1,24 @@
+package cl.bsale.backend.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Entity
+public class Category {
+	
+	@Id
+	private Integer id;
+	private String name; 
+
+}
---
+++ BsaleTest_BackEnd/src/main/java/cl/bsale/backend/model/Product.java
@@ -0,0 +1,32 @@
+package cl.bsale.backend.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Entity
+public class Product {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private String name;
+	private String urlImage;
+	private Float price;
+	private Integer discount;
+	private Category category;
+	
+}
