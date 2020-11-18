be6198fc029d2875c83a542aaaea202e7f3f9c0e
Jose_Arredondo <jotarredondo@gmail.com>
2020-11-18 11:19:36 -0300

    AppCongif - Pool conexiones

--- BsaleTest_BackEnd/src/main/java/cl/bsale/backend/AppConfig.java
+++ BsaleTest_BackEnd/src/main/java/cl/bsale/backend/AppConfig.java
@@ -1,5 +1,36 @@
 package cl.bsale.backend;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+@Configuration
+@ComponentScan("cl.bsale.backend")
+@PropertySource("classpath:database.properties")
 public class AppConfig {
+	
+	//Instancia de ambiente
+	@Autowired
+	Environment ambiente;
+	
+	//Pool de conexiones
+	@Bean
+	public DataSource entregaPool() {
+		
+		DriverManagerDataSource dmds = new DriverManagerDataSource();
+		dmds.setDriverClassName(ambiente.getProperty("driver"));
+		dmds.setUrl(ambiente.getProperty("url"));
+		dmds.setUsername(ambiente.getProperty("usuario"));
+		dmds.setPassword(ambiente.getProperty("clave"));
+		
+		return dmds;
+	}
+	
 
 }
