fb39f3029f876e367f2e89a8557341f62f2ad830
Jose_Arredondo <jotarredondo@gmail.com>
2020-11-19 10:39:08 -0300

    Desarrollo cliente

--- Bsale_Test/src/main/java/cl/bsale/backend/controller/ProductController.java
+++ Bsale_Test/src/main/java/cl/bsale/backend/controller/ProductController.java
@@ -5,7 +5,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 import cl.bsale.backend.service.IProductService;
 
 @Controller
@@ -21,6 +25,15 @@ public class ProductController {
 		
 		modelo.addAttribute("lista", productoServicio.findAll());
 		return "index";
+	}
+	
+	@PostMapping("/buscar")
+	public String buscar(@RequestParam String nombre, Model modelo) {
+		
+		modelo.addAttribute("lista", productoServicio.findByNombre(nombre));
+		
+		return "index";
+		
 	}
 	
 
