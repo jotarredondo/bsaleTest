05e062ccd9957bf75a88f2948e142df0a38b7fb7
Jose_Arredondo <jotarredondo@gmail.com>
2020-11-18 22:58:16 -0300

    Incorporacion Frontend dentro del Proyecto SpringBoot

--- Bsale_Test/src/main/java/cl/bsale/backend/controller/ProductController.java
+++ Bsale_Test/src/main/java/cl/bsale/backend/controller/ProductController.java
@@ -1,53 +1,27 @@
 package cl.bsale.backend.controller;
 
-import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
-import cl.bsale.backend.model.Product;
 import cl.bsale.backend.service.IProductService;
 
 @Controller
-@RestController
-@RequestMapping("/api/v1/producto")
+@RequestMapping("/home")
 public class ProductController {
 	
 	@Autowired
 	IProductService productoServicio;
 	
 	@GetMapping
-	@ResponseStatus(HttpStatus.OK)
-	public List<Product> findAll(){
+	public String inicio(Model modelo){
 		
-		return productoServicio.findAll();
+		modelo.addAttribute("lista", productoServicio.findAll());
+		return "index";
 	}
 	
-	@GetMapping("/{id}")
-	@ResponseStatus(HttpStatus.OK)
-	public Product findById(@PathVariable Integer id){
-		
-		return productoServicio.findById(id);
-	}
-	
-	@GetMapping("/category/{category}")
-	@ResponseStatus(HttpStatus.OK)
-	public List<Product> findByCategory(@PathVariable Integer category){
-		
-		return productoServicio.findByCategory(category);
-	}
-	
-	@GetMapping("/name/{name}")
-	@ResponseStatus(HttpStatus.OK)
-	public List<Product> findByCategory(@PathVariable String name){
-		
-		return productoServicio.findByNombre(name);
-	}
-	
+
 
 }
